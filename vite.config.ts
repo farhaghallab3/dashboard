30ba6a87053675624ed377647fedecff5fee3ff4
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-05-01 02:25:30 +0300

    edit in yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-
 import { defineConfig } from 'vite'
-//import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [react()],
-
-  base: "/dashboard/" 
+  base: '/dashboard/', // Use correct base path for GitHub Pages
+  build: {
+    rollupOptions: {
+      output: {
+        // Ensure assets are correctly named and placed
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      },
+    },
+  },
 })
