566426688f4a72ae3424be50b7f4c52c92e5d4a1
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-05-01 02:42:47 +0300

    edit in vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard/', // Use correct base path for GitHub Pages
+  base: '/', // Use correct base path for GitHub Pages
   build: {
     rollupOptions: {
       output: {
