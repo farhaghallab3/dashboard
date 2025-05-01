4874a9b755584d562b3ef4e43533019934be5106
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-05-01 15:43:41 +0300

    Fix base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use correct base path for GitHub Pages
-  build: {
-    rollupOptions: {
-      output: {
-        // Ensure assets are correctly named and placed
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js',
-      },
-    },
-  },
+  base: '', 
+
 })
